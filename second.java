a1e6a223e2cda82962741b7dafc9cd8932137004
Rajendra Sahoo <rpsahoo@gmail.com>
2022-11-26 03:37:03 -0500

    added sum and second

---
+++ second.java
@@ -0,0 +1,8 @@
+package first;
+
+public class second {
+
+}
+//test
+//test
+//test
---
+++ sum.java
@@ -0,0 +1,10 @@
+package first;
+
+public class sum {
+	int a, b;
+	public void test()
+	{
+		int sum = a+b;
+		
+	}
+}
